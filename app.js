db14593e3f136b34e8772a70b74b5e565259b3e3
Jon <lundbyjon@hotmail.com>
2024-09-06 23:40:43 +0200

    added server request count and time measurement

--- app.js
+++ app.js
@@ -6,32 +6,37 @@ import { getInterval } from "./rest.js";
 window.addEventListener("load", startApp);
 
 function startApp() {
-    console.log("ordbogen frontend kører!");
     document.querySelector("#search-form").addEventListener("submit", searchWord);
-    document.querySelector("#search-results").classList.add("hidden")
-    document.querySelector("#search-results-none").classList.add("hidden")
+    document.querySelector("#search-results").classList.add("hidden");
+    document.querySelector("#search-results-none").classList.add("hidden");
 }
 
 async function searchWord(e) {
     e.preventDefault();
-    
+
     const form = e.target;
     const word = form.searchWord.value.toLowerCase();
-    
+
     const interval = await getInterval();
     const min = interval.min;
     const max = interval.max;
-    
+
+    let startTime = performance.now();
+
     let searchResult = await binarySearch(word, min, max, stringCompareFunction);
-    // console.log(searchResult);
-    
+
+    let endTime = performance.now();
+    let searchTime = ((endTime - startTime) / 1000).toFixed(3);
+
     if (searchResult.result === -1 || searchResult.result === undefined) {
-        console.log("Could not find word.. ")
-        document.querySelector("#search-results").classList.add("hidden")
-        document.querySelector("#search-results-none").classList.remove("hidden")
+        console.log("Could not find word.. ");
+        document.querySelector("#search-results").classList.add("hidden");
+        document.querySelector("#search-results-none").classList.remove("hidden");
+        document.querySelector("#total-time").textContent = searchTime;
     } else {
-        document.querySelector("#search-results-none").classList.add("hidden")
-        document.querySelector("#search-results").classList.remove("hidden")
+        document.querySelector("#search-results-none").classList.add("hidden");
+        document.querySelector("#search-results").classList.remove("hidden");
+        document.querySelector("#total-time").textContent = searchTime;
         document.querySelector("#result-inflected").textContent = searchResult.result.inflected;
         document.querySelector("#result-headword").textContent = searchResult.result.headword;
         document.querySelector("#result-partofspeech").textContent = searchResult.result.partofspeech;
--- binarySearch.js
+++ binarySearch.js
@@ -2,51 +2,48 @@ import { getMiddleWord } from "./rest.js";
 
 async function binarySearch(search, arrMin, arrMax, compFunc) {
     let min = arrMin;
-    let max = arrMax; // skal være -1 fordi ellers mødes min og max aldrig (min & max bliver fx 10 & 11 og det resultere i et endless loop)
+    let max = arrMax;
     let middle;
     let count = 0;
 
     while (min <= max) {
+        document.querySelector("#serverrequests").textContent = count;
         count++;
         middle = Math.floor((max + min) / 2);
 
         let middleWord = await getMiddleWord(middle);
 
         let c = compFunc(search, middleWord.inflected.toLowerCase());
-        console.log(`minValue: ${min} 
-                    maxVal: ${max} 
-                    middle(${middle}): ${middleWord.inflected}  equal with ${search}?
-                    compare: ${c}
-                    serverrequests: ${count}`);
+        console.log(
+            `-minValue: ${min}\n-maxVal: ${max}\n-middle(${middle}): ${middleWord.inflected}  equal with ${search}?\n-compare: ${c}\n-serverrequests: ${count}`
+        );
 
         if (c === 0) {
-            // Hvis search value er lig med middle så returneres middle hvor middle er det index hvor search value er fundet
             return { result: middleWord, count };
         }
         if (c > 0) {
-            // hvis search value er højere end middle så skal middle være ny minimum value
             min = middle + 1;
         }
         if (c < 0) {
-            // hvis search value er lavere end middle så skal middle være ny maximum value
             max = middle - 1;
         }
     }
-    return { result: -1, count }; // returner negativ tal når search value ikke findes i arrayet
+    return { result: -1, count };
 }
 
-function compare(a, b) {
-    return a - b;
-}
+// NOT USED IN THIS EXERCISE
+// function compare(a, b) {
+//     return a - b;
+// }
 
 function stringCompareFunction(a, b) {
-    console.log("compared: " + a + " and " + b);
+    console.log("comparing: " + a + " and " + b);
     return a.localeCompare(b, "da-DK");
 }
 
-function nameCompareFunction(a, b) {
-    console.log("compared: " + a + " and " + b.name);
-    return a.localeCompare(b.name); // a er en string og b er index
-}
+// NOT USED IN THIS EXERCISE
+// function nameCompareFunction(a, b) {
+//     return a.localeCompare(b.name);
+// }
 
-export { binarySearch, compare, stringCompareFunction, nameCompareFunction };
+export { binarySearch, stringCompareFunction };
